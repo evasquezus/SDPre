92530ae602c82cf65df4a1e66828a5652b83a99f
Edwin <giolambo@aol.com>
2018-12-20 19:33:21 -0500

    Completed File Directory

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -2,5 +2,6 @@ public class FirstProg {
   public static void main( String[] args) {
     System.out.println( "I, Edwin Vasquez, am determines to leanr how to code." );
     System.out.println( " Today's date is Thursday, December 20, 2018 ");
+    
   }
 }
