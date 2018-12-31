e621bcbb9b046d86f7a16a9555963191f8d27eec
Edwin <giolambo@aol.com>
2018-12-31 12:23:34 -0700

    Finished Ex 1

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,7 +1,8 @@
 public class FirstProg {
   public static void main( String[] args) {
+
     System.out.println( "I, Edwin Vasquez, am determines to leanr how to code." );
-    System.out.println( " Today's date is Thursday, December 20, 2018 ");   
-    
+    System.out.println( " Today's date is Thursday, December 20, 2018 ");
+        
   }
 }
