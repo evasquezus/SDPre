035096507512e14d1bb9683dcd4d13a1687156cf
Edwin <giolambo@aol.com>
2019-01-06 13:01:57 -0700

    Objective 7 Lab Completed

--- Labs/Objective7Lab3.java
+++ Labs/Objective7Lab3.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
+
 public class Objective7Lab3 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int selection;
 
     while(true){
-      /*
-      Your solution goes here
-      */
+
+      int userChoice;
+
+      printMenu();
+      userChoice= scanner.nextInt();
+
+      if(userChoice == 1) {
+          System.out.println("Hello World ");
+          }
+      else if(userChoice == 2) {
+          System.out.println("Apple, Banana, Coconut ");
+          }
+      else if(userChoice == 3) {
+          System.out.println("Goodbye ");
+          break;
+      }
+      else {
+          System.out.println(" try a number from 1-3 ");
+      }
     }
 
     scanner.close();
